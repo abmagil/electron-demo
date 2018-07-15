f4bd85bdb537657cce3c664aabd2ee1373aead91
Liz <efmorr@gmail.com>
2018-07-15 11:30:17 -0400

    Add Flow to utils/cdf.js

--- src/app/utils/cdf.js
+++ src/app/utils/cdf.js
@@ -1,3 +1,5 @@
+//@flow
+
 import reduce from 'lodash/reduce';
 
 function isNumeric(n) {
@@ -8,10 +10,10 @@ function isArray(arg) {
   return Object.prototype.toString.call( arg ) === '[object Array]';
 }
 
-export default function cdf(arr) {
+export default function cdf(arr: Array<number>): Array<number> {
   if (!isArray(arr)) { return []; }
 
-  const builder = function(acc, n) {
+  const builder = function(acc: Array<number>, n: number): Array<number> {
     if (!isNumeric(n)) { throw new Error('cdf requires an array of numbers'); }
 
     let lastNum = acc.length > 0 ? acc[acc.length - 1] : 0;
